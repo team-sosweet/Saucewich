b919dd2ca38735a2be8f7b2e4257f0f51560f91c
Seokjin Lee <seokjin.dev@gmail.com>
2019-03-30 19:47:27 +0900

    .

--- Source/Saucewich/SaucewichGameMode.h
+++ Source/Saucewich/SaucewichGameMode.h
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+﻿// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
@@ -11,8 +11,6 @@ class ASaucewichGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
-public:
-	ASaucewichGameMode();
 };
 
 
