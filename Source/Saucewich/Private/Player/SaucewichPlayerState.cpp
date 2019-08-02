f652eb23a5e55f5ec11f4e697ac4a036305be5dd
이석진 <seokjin.dev@gmail.com>
2019-08-02 10:45:47 +0900

    몇가지 오류 수정
    1. PlayerState가 GameState보다 먼저 생성되는 경우
    2. Weapons가 nullptr로 채워짐

--- Source/Saucewich/Private/Player/SaucewichPlayerState.cpp
+++ Source/Saucewich/Private/Player/SaucewichPlayerState.cpp
@@ -2,12 +2,15 @@
 
 #include "SaucewichPlayerState.h"
 #include "Engine/World.h"
+#include "TimerManager.h"
 #include "UnrealNetwork.h"
 #include "SaucewichGameMode.h"
 #include "SaucewichGameState.h"
 #include "TpsCharacter.h"
 #include "Weapon.h"
 #include "WeaponComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSaucewichPlayerState, Log, All)
 
 void ASaucewichPlayerState::SetWeapon_Implementation(const uint8 Slot, const TSubclassOf<AWeapon> Weapon)
 {
@@ -22,10 +25,10 @@ bool ASaucewichPlayerState::SetWeapon_Validate(const uint8 Slot, const TSubclass
 void ASaucewichPlayerState::MulticastSetWeapon_Implementation(const uint8 Slot, const TSubclassOf<AWeapon> Weapon)
 {
 	Weapons[Slot] = Weapon;
-	if (const auto GS = GetWorld()->GetGameState<ASaucewichGameState>())
+	SafeGameState([this, Slot, Weapon]
 	{
-		GS->OnPlayerChangedWeapon.Broadcast(this, Slot, Weapon);
-	}
+		GameState->OnPlayerChangedWeapon.Broadcast(this, Slot, Weapon);
+	});
 }
 
 void ASaucewichPlayerState::GiveWeapons()
@@ -52,29 +55,43 @@ void ASaucewichPlayerState::SetTeam(const uint8 NewTeam)
 void ASaucewichPlayerState::OnTeamChanged(const uint8 OldTeam)
 {
 	OnTeamChangedDelegate.Broadcast(Team);
-	if (const auto GS = GetWorld()->GetGameState<ASaucewichGameState>())
+	SafeGameState([this, OldTeam]
 	{
-		GS->OnPlayerChangedTeam.Broadcast(this, OldTeam, Team);
-	}
+		GameState->OnPlayerChangedTeam.Broadcast(this, OldTeam, Team);
+	});
 }
 
 void ASaucewichPlayerState::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 
-	if (const auto Gm = GetWorld()->GetAuthGameMode<ASaucewichGameMode>())
-	{
-		Weapons.Init(
-			nullptr,
-			GetDefault<ATpsCharacter>(
-				Gm->GetDefaultPawnClassForController(static_cast<AController*>(GetOwner()))
-			)->GetWeaponComponent()->GetSlots()
-		);
-	}
+	Init();
+	Weapons = DefaultWeapons;
 }
 
 void ASaucewichPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	DOREPLIFETIME(ASaucewichPlayerState, Team);
+}
+
+void ASaucewichPlayerState::Init()
+{
+	if (const auto GS = GetWorld()->GetGameState())
+	{
+		if (const auto SaucewichGS = Cast<ASaucewichGameState>(GS))
+		{
+			GameState = SaucewichGS;
+			OnGameStateReady.Broadcast();
+			OnGameStateReady.Clear();
+		}
+		else
+		{
+			UE_LOG(LogSaucewichPlayerState, Error, TEXT("이런! 게임 스테이트가 있긴 한데 ASaucewichGameState가 아니네요!"));
+		}
+	}
+	else
+	{
+		GetWorldTimerManager().SetTimerForNextTick(this, &ASaucewichPlayerState::Init);
+	}
 }
--- Source/Saucewich/Public/Player/SaucewichPlayerState.h
+++ Source/Saucewich/Public/Player/SaucewichPlayerState.h
@@ -5,6 +5,7 @@
 #include "GameFramework/PlayerState.h"
 #include "SaucewichPlayerState.generated.h"
 
+DECLARE_MULTICAST_DELEGATE(FOnGameStateReady);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTeamChanged, uint8, NewTeam);
 
 UCLASS()
@@ -37,6 +38,17 @@ protected:
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 private:
+	void Init();
+
+	template <class Fn>
+	void SafeGameState(Fn&& Func)
+	{
+		if (GameState) Func();
+		else OnGameStateReady.AddLambda(std::move(Func));
+	}
+
+	FOnGameStateReady OnGameStateReady;
+
 	// 플레이어가 무기를 선택하지 않았을 때 기본적으로 지급될 무기입니다.
 	// 배열 인덱스는 무기 슬롯을 의미합니다.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
@@ -44,6 +56,8 @@ private:
 
 	UPROPERTY(Transient, EditInstanceOnly, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
 	TArray<TSubclassOf<AWeapon>> Weapons;
+
+	class ASaucewichGameState* GameState;
 
 	UPROPERTY(ReplicatedUsing=OnTeamChanged, Transient, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
 	uint8 Team;
