bfa28e008efa159e4bebbb21be13595eb86df130
이석진 <seokjin.dev@gmail.com>
2019-08-27 18:10:32 +0900

    적 타격 알림을 클라이언트에서 직접 처리

--- Source/Saucewich/Public/Player/SaucewichPlayerController.h
+++ Source/Saucewich/Public/Player/SaucewichPlayerController.h
@@ -5,8 +5,6 @@
 #include "GameFramework/PlayerController.h"
 #include "SaucewichPlayerController.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHitEnemy);
-
 UCLASS()
 class SAUCEWICH_API ASaucewichPlayerController : public APlayerController
 {
@@ -23,10 +21,6 @@ public:
 
 	UFUNCTION(Server, Unreliable, WithValidation)
 	void ServerRespawn();
-
-	// 적에게 피해를 입혔을 때 발동됩니다. 주 목적은 히트마커 입니다.
-	UPROPERTY(BlueprintAssignable)
-	FOnHitEnemy OnHitEnemy;
 
 private:
 	FTimerHandle RespawnTimer;
