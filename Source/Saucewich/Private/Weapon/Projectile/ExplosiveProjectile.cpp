fe967d6b1cbbbbe0708844f2fb1565f90a940cad
이석진 <seokjin.dev@gmail.com>
2019-08-30 00:00:15 +0900

    소스파일 구조 변경

---
+++ Source/Saucewich/Private/Weapon/Projectile/ExplosiveProjectile.cpp
@@ -0,0 +1,4 @@
+// Copyright 2019 Team Sosweet. All Rights Reserved.
+
+#include "Weapon/Projectile/ExplosiveProjectile.h"
+
---
+++ Source/Saucewich/Public/Weapon/Projectile/ExplosiveProjectile.h
@@ -0,0 +1,13 @@
+// Copyright 2019 Team Sosweet. All Rights Reserved.
+
+#pragma once
+
+#include "Weapon/Projectile/Projectile.h"
+#include "ExplosiveProjectile.generated.h"
+
+UCLASS()
+class SAUCEWICH_API AExplosiveProjectile : public AProjectile
+{
+	GENERATED_BODY()
+	
+};
