045063858423781d52a07eb364c42b337bca3293
이석진 <seokjin.dev@gmail.com>
2019-08-30 13:15:43 +0900

    Blueprint의 Damage 관련 함수는 모두 Authority 전용이기 때문에 C++로 이동함

--- Source/Saucewich/Private/Weapon/Projectile/ExplosiveProjectile.cpp
+++ Source/Saucewich/Private/Weapon/Projectile/ExplosiveProjectile.cpp
@@ -2,3 +2,25 @@
 
 #include "Weapon/Projectile/ExplosiveProjectile.h"
 
+#include "Kismet/GameplayStatics.h"
+
+void AExplosiveProjectile::NotifyHit(UPrimitiveComponent* const MyComp, AActor* const Other, UPrimitiveComponent* const OtherComp,
+	const bool bSelfMoved, const FVector HitLocation, const FVector HitNormal, const FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+	
+	UGameplayStatics::ApplyRadialDamage(
+		this,
+		Damage,
+		GetActorLocation(),
+		Radius,
+		DamageType,
+		{},
+		this,
+		GetInstigatorController(),
+		false,
+		DamagePreventionChannel
+	);
+
+	Release();
+}
--- Source/Saucewich/Public/Weapon/Projectile/ExplosiveProjectile.h
+++ Source/Saucewich/Public/Weapon/Projectile/ExplosiveProjectile.h
@@ -6,8 +6,23 @@
 #include "ExplosiveProjectile.generated.h"
 
 UCLASS()
-class SAUCEWICH_API AExplosiveProjectile : public AProjectile
+class SAUCEWICH_API AExplosiveProjectile final : public AProjectile
 {
 	GENERATED_BODY()
+
+protected:
+	void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
 	
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UDamageType> DamageType;
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 100;
+
+	UPROPERTY(EditAnywhere)
+	float Radius = 300;
+
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<ECollisionChannel> DamagePreventionChannel;
 };
