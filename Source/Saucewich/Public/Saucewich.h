d503809464b69daf923af09aae62e6aba8202354
이석진 <seokjin.dev@gmail.com>
2019-08-30 22:14:56 +0900

    Cond 표현식이 한 번만 평가되도록 수정

--- Source/Saucewich/Public/Saucewich.h
+++ Source/Saucewich/Public/Saucewich.h
@@ -16,7 +16,14 @@ DECLARE_LOG_CATEGORY_EXTERN(LogSaucewich, Error, Error)
  * 아래와 같이 if문 안에서도 사용할 수 있습니다. Cond가 참일 경우 코드를 실행합니다.
  * if (GUARANTEE(Cond)) {...}
  */
-#define GUARANTEE(Cond) ((Cond) ? ((void)0) : _LogAssert(TEXT("Assertion failed: " #Cond " in " __FILE__ "(" STRINGIZE(__LINE__) "): " __FUNCTION__)), (Cond))
+#define GUARANTEE(Cond) _Guarantee((Cond), TEXT("Assertion failed: " #Cond " in " __FILE__ "(" STRINGIZE(__LINE__) "): " __FUNCTION__))
+
+template <class T>
+static bool _Guarantee(const bool bCond, T&& Text)
+{
+	if (!bCond) UE_LOG(LogSaucewich, Error, Text);
+	return bCond;
+}
 
 UENUM(BlueprintType)
 enum class EGunTraceHit : uint8
@@ -24,8 +31,3 @@ enum class EGunTraceHit : uint8
 	None, Pawn, Other
 };
 
-template <class T>
-static void _LogAssert(T&& Text)
-{
-	UE_LOG(LogSaucewich, Error, Text);
-}
