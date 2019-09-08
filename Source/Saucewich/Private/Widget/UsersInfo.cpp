512ef895ccee5bb10f65d45718b26034dd3ae620
blAs1N <hwiasin210@naver.com>
2019-09-08 17:26:42 +0900

    Users Info 제작

--- Source/Saucewich/Private/Widget/UsersInfo.cpp
+++ Source/Saucewich/Private/Widget/UsersInfo.cpp
@@ -8,11 +8,13 @@
 
 void UUsersInfo::NativeOnInitialized()
 {
+	Super::NativeOnInitialized();
+	
 	GameState = GetWorld()->GetGameState<ASaucewichGameState>();
 
 	for (auto Index = 0; Index < 8; Index++)
 	{
-		const auto WidgetName = FString::Printf(TEXT("UserInfo_"), Index);
+		const auto WidgetName = FString::Printf(TEXT("UserInfo_%d"), Index);
 		const auto UserInfo = Cast<UUserInfo>(GetWidgetFromName(FName(*WidgetName)));
 		UserInfos.Add(UserInfo);
 	}
@@ -23,26 +25,42 @@ void UUsersInfo::UpdateInfo()
 	auto PlayerArray = GameState->PlayerArray;
 	TArray<ASaucewichPlayerState*> Players;
 	Players.SetNum(PlayerArray.Num());
-
-	auto Index = 0;
 	
-	for (; Index < Players.Num(); Index++)
+	for (auto Index = 0; Index < Players.Num(); Index++)
 	{
 		Players[Index] = Cast<ASaucewichPlayerState>(PlayerArray[Index]);
 	}
 
 	Players.Sort([](const auto& Lhs, const auto& Rhs)
 		{
-			return Lhs.Score > Rhs.Score;
+			if (Lhs.Score != Rhs.Score)
+			{
+				return Lhs.Score > Rhs.Score;
+			}
+			if (Lhs.GetObjective() != Rhs.GetObjective())
+			{
+				return Lhs.GetObjective() > Rhs.GetObjective();
+			}
+			if (Lhs.GetKill() != Rhs.GetKill())
+			{
+				return Lhs.GetKill() > Rhs.GetKill();
+			}
+			if (Lhs.GetDeath() != Rhs.GetDeath())
+			{
+				return Lhs.GetDeath() < Rhs.GetDeath();
+			}
+			return false;
 		});
 
-	for (Index = 0; Index < Players.Num(); Index++)
+	auto Index = 0;
+	
+	for (; Index < Players.Num(); Index++)
 	{
 		UserInfos[Index]->UpdateInfo(Players[Index]);
 		UserInfos[Index]->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
 	}
 
-	for (++Index; Index < UserInfos.Num(); Index++)
+	for (; Index < UserInfos.Num(); Index++)
 	{
 		UserInfos[Index]->SetVisibility(ESlateVisibility::Collapsed);
 	}
