ffdfc9c5903728de4aafdae061821d5b928e224d
이석진 <seokjin.dev@gmail.com>
2019-09-11 18:52:07 +0900

    const 추가

--- Source/Saucewich/Private/Weapon/Projectile/GunProjectile.cpp
+++ Source/Saucewich/Private/Weapon/Projectile/GunProjectile.cpp
@@ -17,7 +17,7 @@ void AGunProjectile::OnActivated()
 	}
 }
 
-void AGunProjectile::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, const bool bSelfMoved,
+void AGunProjectile::NotifyHit(UPrimitiveComponent* const MyComp, AActor* const Other, UPrimitiveComponent* const OtherComp, const bool bSelfMoved,
 	const FVector HitLocation, const FVector HitNormal, const FVector NormalImpulse, const FHitResult& Hit)
 {
 	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
