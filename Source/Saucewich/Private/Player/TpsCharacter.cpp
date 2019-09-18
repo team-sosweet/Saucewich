7b4c92be8bc8ff5a51b3edeca28b490595a66d1c
이석진 <seokjin.dev@gmail.com>
2019-09-18 21:42:46 +0900

    킬로그

--- Source/Saucewich/Private/Player/TpsCharacter.cpp
+++ Source/Saucewich/Private/Player/TpsCharacter.cpp
@@ -24,6 +24,8 @@
 #include "Weapon/WeaponComponent.h"
 #include "ShadowComponent.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogCharacter, Log, All)
+
 ATpsCharacter::ATpsCharacter(const FObjectInitializer& ObjectInitializer)
 	:Super{ObjectInitializer.SetDefaultSubobjectClass<UTpsCharacterMovementComponent>(CharacterMovementComponentName)},
 	WeaponComponent{CreateDefaultSubobject<UWeaponComponent>("WeaponComponent")},
@@ -265,8 +267,18 @@ void ATpsCharacter::Kill(ASaucewichPlayerState* const Attacker, AActor* const In
 	OnCharacterDeath.Broadcast();
 	
 	if (HasAuthority())
+	{
+		const auto MyPS = GetPlayerState();
+		
 		if (const auto GameState = GetWorld()->GetGameState<ASaucewichGameState>())
-			GameState->MulticastPlayerDeath(GetPlayerState<ASaucewichPlayerState>(), Attacker, Inflictor);
+			GameState->MulticastPlayerDeath(Cast<ASaucewichPlayerState>(MyPS), Attacker, Inflictor);
+
+		UE_LOG(LogCharacter, Log, TEXT("%s was killed by %s with %s"),
+			MyPS ? *MyPS->GetPlayerName() : *GetName(),
+			Attacker ? *Attacker->GetPlayerName() : TEXT("unknown"),
+			Inflictor ? *Inflictor->GetName() : TEXT("unknown")
+		);
+	}
 
 	OnKilled();
 }
