125e2c8a8eaaf0a0525da349906afbd59d413acb
blAs1N <hwiasin210@naver.com>
2019-09-20 19:44:03 +0900

    오류 2개 해결

--- Source/Saucewich/Private/GameMode/SaucewichGameState.cpp
+++ Source/Saucewich/Private/GameMode/SaucewichGameState.cpp
@@ -8,7 +8,7 @@
 #include "TimerManager.h"
 #include "UnrealNetwork.h"
 
-#include "Entity/PoolActor.h"
+#include "PoolActor.h"
 #include "Player/SaucewichPlayerController.h"
 #include "Player/SaucewichPlayerState.h"
 #include "Player/TpsCharacter.h"
