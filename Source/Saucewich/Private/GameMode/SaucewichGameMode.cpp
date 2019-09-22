565e9b0295d8238380473bf993cb213c9031f950
이석진 <seokjin.dev@gmail.com>
2019-09-22 21:36:35 +0900

    밸런스 조정

--- Source/Saucewich/Private/GameMode/SaucewichGameMode.cpp
+++ Source/Saucewich/Private/GameMode/SaucewichGameMode.cpp
@@ -76,6 +76,15 @@ APlayerController* ASaucewichGameMode::Login(UPlayer* const NewPlayer, const ENe
 		if (!ID.IsEmpty() && !Token.IsEmpty())
 #endif 
 		{
+			for (const auto OtherPC : TActorRange<ASaucewichPlayerController>{GetWorld()})
+			{
+				if (ID == OtherPC->GetID() || Token == OtherPC->GetToken())
+				{
+					ErrorMessage = TEXT("User with same ID or token has already connected to server");
+					return nullptr;
+				}
+			}
+
 			UE_LOG(LogGameMode, Log, TEXT("New player login. ID: %s, Token: %s"), *ID, *Token);
 			
 			Spc->SetID(ID, Token);
