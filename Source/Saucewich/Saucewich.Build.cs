3a67e0a2c2744552feb5aff6632050c1fc00153d
이석진 <seokjin.dev@gmail.com>
2019-10-21 23:57:26 +0900

    aws gamelift sdk 추가

--- Source/Saucewich/Saucewich.Build.cs
+++ Source/Saucewich/Saucewich.Build.cs
@@ -1,6 +1,7 @@
 // Copyright 2019 Team Sosweet. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class Saucewich : ModuleRules
 {
@@ -9,12 +10,28 @@ public class Saucewich : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Http", "Json", "JsonUtilities", "GameLiftServerSDK" });
-
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        LoadLibraries(new string[] { "aws-cpp-sdk-core", "aws-cpp-sdk-gamelift" });
+	}
+
+	private void LoadLibraries(string[] Libraries)
+	{
+		string ThirdPartyPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/"));
+
+		foreach (string Library in Libraries)
+		{
+			string LibraryPath = ThirdPartyPath + Library + "/";
+			string BinPath = LibraryPath + "bin/";
+			string IncPath = LibraryPath + "include/";
+
+			PublicLibraryPaths.Add(BinPath);
+			PublicIncludePaths.Add(IncPath);
+			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, Library + ".lib"));
+
+			string DLLName = Library + ".dll";
+			PublicDelayLoadDLLs.Add(DLLName);
+			RuntimeDependencies.Add(Path.Combine(LibraryPath, DLLName));
+		}
 	}
 }
