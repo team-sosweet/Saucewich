b9b55bbc3f0df8474bc8194c98cd009c5d678a67
이석진 <seokjin.dev@gmail.com>
2019-11-19 23:22:12 +0900

    소스 겹침 문제 완전 진짜 해결 ㄹㅇ

--- Source/Saucewich/Private/Entity/SauceMarker.cpp
+++ Source/Saucewich/Private/Entity/SauceMarker.cpp
@@ -16,36 +16,43 @@ UInstancedStaticMeshComponent* FSauceMarkers::PickRand() const
 	return Comps[FMath::RandHelper(Comps.Num())];
 }
 
-void ASauceMarker::Add(const uint8 Team, const float Scale, const FHitResult& Hit, const UObject* const WorldContextObj)
+void ASauceMarker::Add(const uint8 Team, const float Scale, const FHitResult& Hit, const AActor* const Ignore)
 {
 #if !UE_SERVER
-	const auto World = WorldContextObj->GetWorld();
+	const auto World = Ignore->GetWorld();
 
 	auto Rot = Hit.ImpactNormal.ToOrientationQuat();
 	Rot *= FRotator{-90.f, 0.f, 0.f}.Quaternion();
 	Rot *= FRotator{0.f, 360.f * FMath::FRand(), 0.f}.Quaternion();
 
-	const auto Loc = Hit.ImpactPoint + Hit.ImpactNormal * .01f;
-
 	const auto RandScale = [&]{return Scale * FMath::RandRange(.85f, 1.15f);};
 	const FVector Scale3D{RandScale(), RandScale(), 1.f};
+
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(Ignore);
+
+	auto MaxOffset = 0.f;
 
 	const auto LineTraceTest = [&](const float AxisScale, auto&&... Dirs)
 	{
 		for (auto&& Dir : {Forward<decltype(Dirs)>(Dirs)...})
 		{
-			const auto Start = Loc + Rot.RotateVector(Dir) * (AxisScale * 50.f);
-			if (!World->LineTraceTestByChannel(Start, Start - Hit.ImpactNormal, ECC_Visibility)) return false;
+			const auto Offset = Rot.RotateVector(Dir) * (AxisScale * 50.f);
+			const auto Loc = Hit.ImpactPoint + Offset;
+			FHitResult H;
+			if (!World->LineTraceSingleByChannel(H, Loc + Hit.ImpactNormal, Loc - Hit.ImpactNormal, ECC_Visibility, Params)) return false;
+			MaxOffset = FMath::Max(1.f - H.Distance, MaxOffset);
 		}
 		return true;
 	};
 
 	if (!LineTraceTest(Scale3D.X, FVector::ForwardVector, FVector::BackwardVector)) return;
 	if (!LineTraceTest(Scale3D.Y, FVector::RightVector, FVector::LeftVector)) return;
+	if (!LineTraceTest(0.f, FVector::ZeroVector)) return;
 	
 	const auto Marker = USaucewichInstance::Get(World)->GetSauceMarker();
 	const auto Comp = Marker->TeamMarkers[Team].PickRand();
-	Comp->AddInstanceWorldSpace({Rot, Loc, Scale3D});
+	Comp->AddInstanceWorldSpace({Rot, Hit.ImpactPoint + Hit.ImpactNormal * (MaxOffset + .01f), Scale3D});
 #endif
 }
 
