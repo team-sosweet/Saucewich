509cf9fc02dec315e32a59a186f85a4de3be72a2
이석진 <seokjin.dev@gmail.com>
2019-11-30 23:40:32 +0900

    세션 찾기

--- Source/Saucewich/Private/Saucewich.cpp
+++ Source/Saucewich/Private/Saucewich.cpp
@@ -4,6 +4,10 @@
 
 #include "EngineUtils.h"
 #include "Modules/ModuleManager.h"
+#include "Http.h"
+#include "Json.h"
+#include "JsonObjectConverter.h"
+#include "HttpManager.h"
 
 #include "GameLiftServerSDK.h"
 
@@ -16,6 +20,26 @@ FGameLiftServerSDKModule& USaucewich::GetGameLift()
 {
 	static auto& Module = FModuleManager::GetModuleChecked<FGameLiftServerSDKModule>(TEXT("GameLiftServerSDK"));
 	return Module;
+}
+
+void USaucewich::SearchSession(const FSearchSessionResponse& Callback)
+{
+	Request(TEXT("GET"), TEXT("http://localhost:6974/session"), FOnHttpResponse::CreateLambda(
+		[=](const int32 ResponseCode, const FJsonObject& Content)
+		{
+			if (ResponseCode == 200)
+			{
+				const auto ServerIP = Content.GetStringField(TEXT("server-ip"));
+				const auto PlayerID = Content.GetStringField(TEXT("player-id"));
+				if (!PlayerID.IsEmpty() && !ServerIP.IsEmpty())
+				{
+					(void)Callback.ExecuteIfBound(true, ServerIP, PlayerID);
+					return;
+				}
+			}
+			(void)Callback.ExecuteIfBound(false, {}, {});
+		}
+	));
 }
 
 ENameValidity USaucewich::IsValidPlayerName(const FString& PlayerName)
@@ -38,4 +62,50 @@ int32 USaucewich::GetPlayerNameMinLen()
 int32 USaucewich::GetPlayerNameMaxLen()
 {
 	return 16;
+}
+
+static TSharedRef<IHttpRequest> CreateRequest(const FString& Verb, const FString& URL, const FOnHttpResponse& OnResponse)
+{
+	const auto Request = FHttpModule::Get().CreateRequest();
+	Request->SetVerb(Verb);
+	Request->SetURL(URL);
+
+	Request->OnProcessRequestComplete().BindLambda(
+		[=](const FHttpRequestPtr&, const FHttpResponsePtr& Response, const bool bConnectedSuccessfully)
+		{
+			if (bConnectedSuccessfully && Response)
+			{
+				const auto Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+				TSharedPtr<FJsonObject> JsonObject;
+				FJsonSerializer::Deserialize(Reader, JsonObject);
+				(void)OnResponse.ExecuteIfBound(Response->GetResponseCode(), JsonObject ? *JsonObject : FJsonObject{});
+			}
+			else
+			{
+				(void)OnResponse.ExecuteIfBound(0, {});
+			}
+		}
+	);
+
+	return Request;
+}
+
+void USaucewich::Request(const FString& Verb, const FString& URL, const FOnHttpResponse& OnResponse)
+{
+	const auto Request = CreateRequest(Verb, URL, OnResponse);
+	if (!Request->ProcessRequest()) OnResponse.ExecuteIfBound(0, {});
+}
+
+void USaucewich::Request(const FString& Verb, const FString& URL, const TSharedRef<FJsonObject>& Content, const FOnHttpResponse& OnResponse)
+{
+	const auto Request = CreateRequest(Verb, URL, OnResponse);
+	
+	FString Serialized;
+	const auto Writer = TJsonWriterFactory<>::Create(&Serialized);
+	if (FJsonSerializer::Serialize(Content, Writer))
+	{
+		Request->SetContentAsString(Serialized);
+	}
+
+	if (!Request->ProcessRequest()) OnResponse.ExecuteIfBound(0, {});
 }
--- Source/Saucewich/Public/Saucewich.h
+++ Source/Saucewich/Public/Saucewich.h
@@ -6,6 +6,16 @@
 #include "Engine/EngineTypes.h"
 #include "Saucewich.generated.h"
 
+#define FMT_MSG(Fmt, ...) [&]{static const FTextFormat Compiled{Fmt};return FText::FormatOrdered(Compiled,__VA_ARGS__);}()
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSaucewich, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogGameLift, Log, All)
+
+class FJsonObject;
+
+DECLARE_DYNAMIC_DELEGATE_ThreeParams(FSearchSessionResponse, bool, bFound, const FString&, URL, const FString&, PlayerID);
+DECLARE_DELEGATE_TwoParams(FOnHttpResponse, int32, const FJsonObject&)
+
 UENUM(BlueprintType)
 enum class EGunTraceHit : uint8
 {
@@ -17,12 +27,6 @@ enum class EMsgType : uint8
 {
 	Center, Left
 };
-
-DECLARE_LOG_CATEGORY_EXTERN(LogSaucewich, Log, All)
-
-DECLARE_LOG_CATEGORY_EXTERN(LogGameLift, Log, All)
-
-#define FMT_MSG(Fmt, ...) [&]{static const FTextFormat Compiled{Fmt};return FText::FormatOrdered(Compiled,__VA_ARGS__);}()
 
 UENUM(BlueprintType)
 enum class ENameValidity : uint8
@@ -36,6 +40,9 @@ class SAUCEWICH_API USaucewich : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable)
+	static void SearchSession(const FSearchSessionResponse& Callback);
+	
 	UFUNCTION(BlueprintPure)
 	static ENameValidity IsValidPlayerName(const FString& PlayerName);
 
@@ -46,4 +53,7 @@ public:
 	static int32 GetPlayerNameMaxLen();
 
 	static class FGameLiftServerSDKModule& GetGameLift();
+
+	static void Request(const FString& Verb, const FString& URL, const FOnHttpResponse& OnResponse);
+	static void Request(const FString& Verb, const FString& URL, const TSharedRef<FJsonObject>& Content, const FOnHttpResponse& OnResponse);
 };
