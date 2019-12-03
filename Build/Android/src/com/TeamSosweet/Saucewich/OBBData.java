3b122f3281f358c3e178200fcfbc1d7061156176
이석진 <seokjin.dev@gmail.com>
2019-12-04 06:29:25 +0900

    광고 수정

--- Build/Android/src/com/TeamSosweet/Saucewich/OBBData.java
+++ Build/Android/src/com/TeamSosweet/Saucewich/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "9", // the version of the APK that the file was uploaded against
-47224044L // the length of the file in bytes
+47223705L // the length of the file in bytes
 )
 };
 };
