a0f8ef72f71d22724a219e72da0ce096294a4e7e
이석진 <seokjin.dev@gmail.com>
2019-12-10 11:11:04 +0900

    .

--- Source/Saucewich/Public/TutorialManager.h
+++ Source/Saucewich/Public/TutorialManager.h
@@ -5,10 +5,20 @@
 #include "GameFramework/SaveGame.h"
 #include "TutorialManager.generated.h"
 
+USTRUCT()
+struct FTutorial
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	uint8 MaxCount = 2;
+
+	UPROPERTY()
+	uint8 Count;
+};
+
 UCLASS()
 class SAUCEWICH_API UTutorialManager : public USaveGame
 {
 	GENERATED_BODY()
-	
-
 };
