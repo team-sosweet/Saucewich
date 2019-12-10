8445a5c4ec02c061db85fed7f8e85860fc4f8d42
이석진 <seokjin.dev@gmail.com>
2019-12-10 10:29:28 +0900

    튜토리얼 매니저 추가

---
+++ Source/Saucewich/Private/TutorialManager.cpp
@@ -0,0 +1,4 @@
+// Copyright 2019 Othereum. All Rights Reserved.
+
+#include "TutorialManager.h"
+
---
+++ Source/Saucewich/Public/TutorialManager.h
@@ -0,0 +1,14 @@
+// Copyright 2019 Othereum. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/SaveGame.h"
+#include "TutorialManager.generated.h"
+
+UCLASS()
+class SAUCEWICH_API UTutorialManager : public USaveGame
+{
+	GENERATED_BODY()
+	
+
+};
