6ece54dbdcac52639751ab83f61487344c9ab602
Othereum <seokjin.dev@gmail.com>
2020-01-12 20:30:27 +0900

    갤럭시 S6 실행문제 해결

--- Source/Saucewich.Target.cs
+++ Source/Saucewich.Target.cs
@@ -8,6 +8,7 @@ public class SaucewichTarget : TargetRules
 	public SaucewichTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		bUseLoggingInShipping = true;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "Saucewich" } );
 	}
